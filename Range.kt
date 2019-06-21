b9c531bf59288c8b0424fcc82c03755308c5fa75
Fahri Can <fahri-can@gmx.at>
2019-06-21 21:28:07 +0200

    Create Range.kt

---
+++ Range.kt
@@ -0,0 +1,38 @@
+package com.example.rxjavamasterclass
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dataStream = Observable.range(30, 11)
+
+        val dataObserver = object : Observer<Int> {
+            override fun onSubscribe(d: Disposable) {
+                println("Observer subscribed to Observable")
+            }
+
+            override fun onNext(t: Int) {
+                println("new data is received: $t")
+
+            }
+
+            override fun onComplete() {
+                println("All data is received!")
+            }
+
+            override fun onError(e: Throwable) {
+                println("A error is received ${e.message}")
+            }
+        }
+        dataStream.subscribe(dataObserver)
+    }
+}
