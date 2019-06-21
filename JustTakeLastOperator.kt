a105ee7f4f743449f52f0d549620c54b5655a5c6
Fahri Can <fahri-can@gmx.at>
2019-06-21 22:43:30 +0200

    Create JustTakeLastOperator.kt

---
+++ JustTakeLastOperator.kt
@@ -0,0 +1,39 @@
+package com.example.rxjavamasterclass
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dataStream = Observable.just(10, 20, 50, 80)
+            .takeLast(3)
+
+        val dataObserver = object : Observer<Int> {
+            override fun onSubscribe(d: Disposable) {
+                println("Observer subscribed to Observable")
+            }
+
+            override fun onNext(t: Int) {
+                println("Item received $t")
+            }
+
+            override fun onComplete() {
+                println("All data is received!")
+            }
+
+            override fun onError(e: Throwable) {
+                println("A error is received ${e.message}")
+            }
+        }
+        dataStream.subscribe(dataObserver)
+    }
+}
